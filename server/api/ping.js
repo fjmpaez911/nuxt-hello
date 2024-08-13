8f21c538010b6661f7baf25d14c538bc60c9d1b1
Francisco José Martínez Páez <fj.martinez.paez@gmail.com>
2024-08-13 17:14:47 +0200

    Update ping.js

--- server/api/ping.js
+++ server/api/ping.js
@@ -1,6 +1,3 @@
-export default defineEventHandler(async (event) => {
-  const uri = `https://jsonplaceholder.typicode.com/todos/1`;
-  const { data } = await $fetch(uri);
-
-  return data;
+export default defineEventHandler(() => {
+  return { message: 'pong' };
 });
